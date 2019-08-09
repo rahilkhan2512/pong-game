e69d95faba4040c7388f7261a463cbd0dfe2c146
Sohel Shaikh <sohelshaikh97@gmail.com>
2019-08-09 18:01:02 +0530

    added basic movement

--- pong.py
+++ pong.py
@@ -8,7 +8,75 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0) # stops the window from updating
 
-# Main game loop
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) # assigns maximus possible speed
+paddle_a.shape("square")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
 
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) # assigns maximus possible speed
+paddle_b.shape("square")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0) # assigns maximus possible speed
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
+
+# Game Functions
+def paddle_a_up():
+    y = paddle_a.ycor() # returns y coordinate
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor() # returns y coordinate
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor() # returns y coordinate
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor() # returns y coordinate
+    y -= 20
+    paddle_b.sety(y)
+
+# Keyboard Binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+# Main game loop
 while True:
     wn.update()
+
+    # Movement for the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Defining borders
+    if(ball.ycor() > 290):
+        ball.sety(290)
+        ball.dy *= -1
+
+    if(ball.ycor() < -290):
+        ball.sety(290)
+        ball.dy *= -1
